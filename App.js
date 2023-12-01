e7cb6645b2e9c29cc703465ca222fbf14d4a90c2
lucasjlgc <lucasjlgc@outlook.com>
2023-12-01 15:55:57 -0300

    Initial commit
    Generated by create-expo-app 2.1.1.

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+ 
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+ 
+  },
+});
